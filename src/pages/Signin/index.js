59fbcff6ffafd5d867031b5cc1a6d696c4d477e0
Alan Bruno Ferreira da Silva <alanjjr83@gmail.com>
2021-05-06 20:42:58 -0300

    Animation on Login Page

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -19,8 +19,33 @@ function SignIn() {
     alert('CLICOU')
   }
 
+  function squares() {
+      const ulSquares = document.querySelector('ul.squares');
+
+      for(let i = 0; i < 11; i++) {
+        const li = document.createElement('li');
+
+        const random = (min, max) => Math.random() * (max - min) + min;
+
+        const size = Math.floor(random(10, 120));
+        const position = random(-10, 100);
+        const delay = random(5, 0.1);
+        const duration = random(24, 12);
+
+        li.style.width = `${size}px`;
+        li.style.height = `${size}px`;
+        li.style.left = `${position}%`;
+        li.style.right = `${position}%`;
+        li.style.animationDelay = `${delay}s`;
+        li.style.animationDuration = `${duration}s`;
+        li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`
+
+        ulSquares.appendChild(li);
+      }
+  }
+  
   return (
-    <div className="container-center">
+    <div className="container-center" onLoad={squares}>
       <div className="login">
         <div className="logo-area">
           <h1>Pague Fácil</h1>
@@ -41,6 +66,10 @@ function SignIn() {
           </span>
         </Link>
       </div>
+
+      <ul className="squares">
+        <li></li>
+      </ul>
     </div>
   )
 }
