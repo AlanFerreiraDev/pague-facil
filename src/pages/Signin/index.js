d5d34b250c9e5a6fde01d180f46e2d8e80f2fce0
Alan Bruno Ferreira da Silva <alanjjr83@gmail.com>
2021-05-06 17:34:23 -0300

    Page Register Done

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 //css
-import './sigin.css';
+import './signin.css';
 
 //img
 import logo from '../../assets/logo.png';
@@ -14,6 +14,11 @@ function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert('CLICOU')
+  }
+
   return (
     <div className="container-center">
       <div className="login">
@@ -22,15 +27,15 @@ function SignIn() {
           <img src={logo} alt="Pague Fácil Logo" width="10px"/>
         </div>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <h2>Entrar</h2>
-          <input type="text" placeholder="email@email.com" />
-          <input type="password" placeholder="********"/>
+          <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.value)} />
+          <input type="password" placeholder="********" value={password} onChange={ (e) => setPassword(e.target.value)} />
           <button type="submit">Acessar sua Conta</button>
         </form>
 
         <Link to="/register">
-          Faça Parte dos PagLovers e Crie uma conta agora   
+          Faça parte dos <strong>Pag Lovers</strong> e abra uma conta agora  
           <span>
             <img src={moeda} alt="Coracao Pague Facil"/>
           </span>
--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,7 +1,49 @@
+//react
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+//css
+import './signup.css';
+
+//img
+import logo from '../../assets/logo.png';
+import moeda from '../../assets/spanMoeda.png';
 
 function SignUp() {
+  //hooks
+  const [nome, setNome] = useState();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert('CLICOU')
+  }
+
   return (
-    <h1>PAGINA DE CADASTRO</h1>
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <h1>Pague Fácil</h1>
+          <img src={logo} alt="Pague Fácil Logo" width="10px"/>
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h2>Cadastre sua Conta aqui</h2>
+          <input type="text" placeholder="Seu Nome" vlaue={nome} onChange={ (e) => setNome(e.target.value)}/>
+          <input type="text" placeholder="email@email.com" value={email} onChange={ (e) => setEmail(e.target.value)} />
+          <input type="password" placeholder="********" value={password} onChange={ (e) => setPassword(e.target.value)} />
+          <button type="submit">Cadastrar</button>
+        </form>
+
+        <Link to="/">
+          Já faz parte dos <strong>Pag Lovers ?</strong> Entre 
+          <span>
+            <img src={moeda} alt="Coracao Pague Facil"/>
+          </span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
